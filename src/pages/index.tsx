508682d1db3c5e27485ff0d17bfa14204f5e505b
John Letey <johnletey@gmail.com>
2021-04-06 16:34:08 +0100

    feat(wip): pull top communities

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,30 @@
-const Home = () => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Home = (props: { communities: any }) => {
+  const [communities, setCommunities] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://v2.cache.verto.exchange/site/communities")
+      .then((res) => setCommunities(res.data));
+  }, []);
+
+  console.log(communities);
+
   return <></>;
 };
+
+// export async function getServerSideProps() {
+//   const { data: communities } = await axios.get(
+//     "https://v2.cache.verto.exchange/site/communities"
+//   );
+
+//   return {
+//     props: {
+//       communities,
+//     },
+//   };
+// }
 
 export default Home;
