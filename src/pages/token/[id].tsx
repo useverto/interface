8f1481f61140822fb5971f651e3f15ed3b85826b
John Letey <johnletey@gmail.com>
2021-04-10 16:39:05 +0100

    feat: fetch price history as well

--- src/pages/token/[id].tsx
+++ src/pages/token/[id].tsx
@@ -1,23 +1,35 @@
 import Verto from "@verto/js";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Token = (props: { price: number; name: string; ticker: string }) => {
-  console.log(props);
+const client = new Verto();
+
+const Token = (props: {
+  id: string;
+  price: number;
+  name: string;
+  ticker: string;
+}) => {
+  const [history, setHistory] = useState({});
+
+  useEffect(() => {
+    client.getPriceHistory(props.id).then((res) => setHistory(res));
+  }, []);
+
+  console.log(history);
 
   return <></>;
 };
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
-
-  const client = new Verto();
   const res = await client.getPrice(id);
 
   const { data: gecko } = await axios.get(
     "https://api.coingecko.com/api/v3/simple/price?ids=arweave&vs_currencies=usd"
   );
 
-  return { props: { ...res, price: res.price * gecko.arweave.usd } };
+  return { props: { id, ...res, price: res.price * gecko.arweave.usd } };
 }
 
 export default Token;
