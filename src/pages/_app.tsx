7c429516a356579552e95ff44648e0329da5c0a7
John Letey <johnletey@gmail.com>
2021-04-10 14:21:46 +0100

    feat: typing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { VertoProvider } from "@verto/ui";
 
-export default function App ({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <VertoProvider theme={"Light"}>
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
 import { Button, Card, Page, Spacer } from "@verto/ui";
 import styles from "../styles/views/home.module.sass";
+import Typed from "typed.js";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    const options = {
+      strings: ["PSTs", "PSCs", "NFTs", "collectibles", "anything"],
+      showCursor: false,
+
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      smartBackspace: false,
+    };
+
+    const typed = new Typed("#typed", options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
 
   return (
     <Page>
@@ -9,14 +28,23 @@ const Home = () => {
       <div className={styles.Landing}>
         <div className={styles.Hero}>
           <h1>
-            Exchange PSTs
-            <br/>
+            Exchange <span id="typed" />
+            <br />
             on Arweave
           </h1>
           <Spacer y={1.35} />
           <p>
-            Verto is a decentralized trading protocol<br/>
-            built on top of <a href="https://arweave.org" target="_blank" rel="noopener noreferrer">Arweave</a>.
+            Verto is a decentralized trading protocol
+            <br />
+            built on top of{" "}
+            <a
+              href="https://arweave.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Arweave
+            </a>
+            .
           </p>
           <Spacer y={1.35} />
           <div className={styles.HeroBtns}>
@@ -24,9 +52,7 @@ const Home = () => {
               Explore tokens
             </Button>
             <Spacer x={1} />
-            <Button small>
-              Trade now
-            </Button>
+            <Button small>Trade now</Button>
           </div>
         </div>
         <div className={styles.FeaturedToken}>
@@ -35,7 +61,7 @@ const Home = () => {
             userData={{
               avatar: "https://th8ta.org/marton.jpeg",
               name: "Marton Lederer",
-              usertag: "martonlederer"
+              usertag: "martonlederer",
             }}
             price={125}
             image="https://raw.githubusercontent.com/useverto/ui/v2/test/public/art.png"
@@ -45,6 +71,6 @@ const Home = () => {
       <Spacer y={5} />
     </Page>
   );
-}
+};
 
 export default Home;
