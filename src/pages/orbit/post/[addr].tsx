71f805a9c8296677c444259ce2f5c585540e77fe
John Letey <johnletey@gmail.com>
2021-04-13 20:06:43 +0100

    feat: start using pagination on Orbit post page

--- src/pages/orbit/post/[addr].tsx
+++ src/pages/orbit/post/[addr].tsx
@@ -1,14 +1,33 @@
-import { Card, Page } from "@verto/ui";
+import { Button, Card, Page, Spacer } from "@verto/ui";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-const Post = (props: { orders: any[] }) => {
+const Post = (props: { addr: string; orders: any[] }) => {
+  const [orders, setOrders] = useState(props.orders);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (loading && props.orders.length) {
+      axios
+        .get(
+          `https://v2.cache.verto.exchange/posts/${
+            props.addr
+          }/orders?limit=10&after=${props.orders[props.orders.length - 1].id}`
+        )
+        .then((res) => {
+          setOrders((val) => [...val, ...res.data]);
+          setLoading(false);
+        });
+    }
+  }, [loading]);
+
   return (
     <Page>
-      {props.orders.map((order) => {
+      {orders.map((order) => {
         let status = order.status;
         if (status === "success" || status === "pending") {
           // don't do anything
-        } else if (status === "cancelled") {
+        } else if (status === "cancelled" || status === "refunded") {
           status = "neutral";
         } else {
           status = "error";
@@ -22,14 +41,21 @@ const Post = (props: { orders: any[] }) => {
         }
 
         return (
-          <Card.Order
-            type={type}
-            orderID={order.id}
-            status={status}
-            timestamp={new Date(order.timestamp * 1000)}
-          />
+          <>
+            <Card.Order
+              type={type}
+              orderID={order.id}
+              status={status}
+              timestamp={new Date(order.timestamp * 1000)}
+              key={order.id}
+            />
+            <Spacer y={1} />
+          </>
         );
       })}
+      <Button loading={loading} onClick={() => setLoading(true)}>
+        Load More
+      </Button>
     </Page>
   );
 };
@@ -38,10 +64,10 @@ export async function getServerSideProps(context) {
   const { addr } = context.query;
 
   const { data: orders } = await axios.get(
-    `https://v2.cache.verto.exchange/posts/${addr}/orders`
+    `https://v2.cache.verto.exchange/posts/${addr}/orders?limit=10`
   );
 
-  return { props: { orders } };
+  return { props: { addr, orders } };
 }
 
 export default Post;
