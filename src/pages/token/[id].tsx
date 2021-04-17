0321453f3c376f724e58712829622ac7714ec814
John Letey <johnletey@gmail.com>
2021-04-17 18:28:30 +0100

    feat(wip): implement transferring on token page

--- src/pages/token/[id].tsx
+++ src/pages/token/[id].tsx
@@ -1,5 +1,6 @@
 import Verto from "@verto/js";
 import axios from "axios";
+import { Button, Card, Input, Spacer, useInput } from "@verto/ui";
 import { useEffect, useState } from "react";
 
 const client = new Verto();
@@ -15,10 +16,49 @@ const Token = (props: {
   useEffect(() => {
     client.getPriceHistory(props.id).then((res) => setHistory(res));
   }, []);
-
   console.log(history);
 
-  return <></>;
+  const amount = useInput();
+  const target = useInput();
+
+  const transfer = async () => {
+    const id = await client.transfer(
+      amount.state as number,
+      props.id,
+      target.state as string
+    );
+    console.log(id);
+  };
+
+  return (
+    <>
+      <Card>
+        <Input
+          label="Address"
+          placeholder="Your address"
+          style={{ width: "calc(100% - 6px)" }}
+          {...target.bindings}
+        />
+        <Spacer y={1} />
+        <Input
+          label="You send"
+          inlineLabel={props.ticker}
+          type="number"
+          placeholder="10000"
+          style={{ width: "calc(100% - 6px)" }}
+          {...amount.bindings}
+        />
+        <Spacer y={2} />
+        <Button style={{ width: "100%" }} disabled onClick={transfer}>
+          Transfer
+        </Button>
+        <Spacer y={1} />
+        <Button style={{ width: "100%" }} disabled type="secondary">
+          Swap
+        </Button>
+      </Card>
+    </>
+  );
 };
 
 export async function getServerSideProps(context) {
