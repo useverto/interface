53df3fc60ce4d23bf491c6ad2f1a97e913f8111d
John Letey <johnletey@gmail.com>
2021-04-24 19:10:18 +0100

    feat: add some metrics for communities

--- src/pages/space/[id].tsx
+++ src/pages/space/[id].tsx
@@ -13,12 +13,55 @@ interface PropTypes extends PriceInterface {
 const Token = (props: PropTypes) => {
   const type = props.type || "community";
 
+  const [state, setState] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://v2.cache.verto.exchange/${props.id}`)
+      .then(({ data }) => {
+        let state = data.state;
+        if (state.settings)
+          state.settings = Object.fromEntries(new Map(state.settings));
+
+        setState(state);
+      });
+  }, []);
+
   // === Community ===
   const [history, setHistory] = useState<{ [date: string]: number }>({});
 
   useEffect(() => {
     client.getPriceHistory(props.id).then((res) => setHistory(res));
   }, []);
+
+  const [metrics, setMetrics] = useState(null);
+
+  useEffect(() => {
+    if (state) {
+      const circulatingSupply = Object.values(state.balances).reduce(
+        (a: number, b: number) => a + b,
+        0
+      ) as number;
+
+      let totalSupply: number = circulatingSupply;
+      for (const vault of Object.values(state.vault) as any) {
+        totalSupply += vault
+          .map((a: any) => a.balance)
+          .reduce((a: number, b: number) => a + b, 0);
+      }
+
+      const marketCap = (totalSupply * props.price).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+
+      setMetrics({
+        marketCap,
+        circulatingSupply: circulatingSupply.toLocaleString(),
+        totalSupply: totalSupply.toLocaleString(),
+      });
+    }
+  }, [state]);
 
   const amount = useInput();
   const target = useInput();
@@ -35,6 +78,17 @@ const Token = (props: PropTypes) => {
   return (
     <Page>
       {type === "art" && <img src={`https://arweave.net/${props.id}`} />}
+      {type === "community" && metrics && (
+        <>
+          <p>Market Cap: ~${metrics.marketCap} USD</p>
+          <p>
+            Circulating Supply: {metrics.circulatingSupply} {props.ticker}
+          </p>
+          <p>
+            Total Supply: {metrics.totalSupply} {props.ticker}
+          </p>
+        </>
+      )}
     </Page>
   );
 };
