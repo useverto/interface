6e68850fb27644c8e6b705aa41ceab51a5a51fc2
Marton Lederer <marton@lederer.hu>
2021-07-06 15:07:23 +0200

    chore: update community contract

--- src/utils/arweave.ts
+++ src/utils/arweave.ts
@@ -14,7 +14,7 @@ const gql = new ArDB(client);
 
 export const ROOT_URL = "https://vext.vercel.app";
 export const CACHE_URL = "https://v2.cache.verto.exchange";
-export const COMMUNITY_CONTRACT = "yxdwrWJiaDDwKHIridjuBhtSR_IF7K8B8euJRLqaTrw";
+export const COMMUNITY_CONTRACT = "Wgp3SpOtWXGCi1SardwQAwrOsQeW6aQn36ooo6eo8nc";
 export const INVITE_CONTRACT = ""; // TODO
 
 export const balanceHistory = async (
