a0aa7a2cfca9ea7495b457447876334a4a12cf84
Marton Lederer <marton@lederer.hu>
2021-08-10 13:27:32 +0200

    fix: rewrites & redirects

--- next.config.js
+++ next.config.js
@@ -27,6 +27,15 @@ module.exports = {
         destination: "/@:user",
         permanent: true,
       },
+      {
+        source: "/roadmap",
+        destination:
+          "https://www.notion.so/verto/48f99dfb0ba744f5b5257185f53ad7c4",
+      },
+      {
+        source: "/feedback",
+        destination: "https://forms.gle/m3TJDfg1HRpV994Y8",
+      },
     ];
   },
   async rewrites() {
@@ -50,6 +59,30 @@ module.exports = {
       {
         source: "/(@):user/owns",
         destination: "/user/:user/owns",
+      },
+      {
+        source: "/i/win",
+        destination:
+          "https://raw.githubusercontent.com/useverto/trading-post/master/install/windows.ps1",
+      },
+      {
+        source: "/i/mac",
+        destination:
+          "https://raw.githubusercontent.com/useverto/trading-post/master/install/mac.sh",
+      },
+      {
+        source: "/i/linux",
+        destination:
+          "https://raw.githubusercontent.com/useverto/trading-post/master/install/linux.sh",
+      },
+      {
+        source: "/chat",
+        destination: "https://discord.gg/sNgJkMg",
+      },
+      {
+        source: "/link",
+        destination:
+          "https://arweave.net/CodqSDWXY5CALyMf9oFLCtTDRYdW4lV9X9O7j-73g1U",
       },
     ];
   },
