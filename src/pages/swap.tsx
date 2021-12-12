04c21fdabd73beeaa024938146b190496424d46e
Marton Lederer <marton@lederer.hu>
2021-12-12 18:04:12 +0100

    feat: new swap form wip

--- src/pages/swap.tsx
+++ src/pages/swap.tsx
@@ -1,11 +1,25 @@
 import { UserInterface } from "@verto/js/dist/faces";
-import { Button, Page, Select, Spacer, useModal, Modal } from "@verto/ui";
+import {
+  Button,
+  Page,
+  Select,
+  Spacer,
+  useModal,
+  Modal,
+  useTheme,
+  Card,
+} from "@verto/ui";
 import { useEffect } from "react";
 import { permissions as requiredPermissions } from "../utils/arconnect";
+import { GraphDataConfig, GraphOptions } from "../utils/graph";
+import { AnimatePresence, motion } from "framer-motion";
+import { Line } from "react-chartjs-2";
+import { ChevronDownIcon } from "@iconicicons/react";
 import Balance from "../components/Balance";
 import Head from "next/head";
 import Metas from "../components/Metas";
 import useArConnect from "use-arconnect";
+import useGeofence from "../utils/geofence";
 import styles from "../styles/views/swap.module.sass";
 
 const Swap = () => {
@@ -33,6 +47,12 @@ const Swap = () => {
     })();
   }, [arconnect]);
 
+  // display theme
+  const theme = useTheme();
+
+  // geofence
+  const blockedCountry = useGeofence();
+
   return (
     <Page>
       <Head>
@@ -42,7 +62,94 @@ const Swap = () => {
       <Spacer y={3} />
       <Balance />
       <Spacer y={4} />
-      <div className={styles.SwapContent}></div>
+      <div className={styles.SwapContent}>
+        <div className={styles.Graph}>
+          <Line
+            data={{
+              // TODO: filterGraphData()
+              labels: [
+                "2021-12-01",
+                "2021-12-02",
+                "2021-12-03",
+                "2021-12-04",
+                "2021-12-05",
+                "2021-12-06",
+                "2021-12-07",
+              ],
+              datasets: [
+                {
+                  // TODO: filterGraphData()
+                  data: [0, 1, 2, 2, 4, 3, 2, 4],
+                  ...GraphDataConfig,
+                  borderColor: theme === "Light" ? "#000000" : "#ffffff",
+                },
+              ],
+            }}
+            options={{
+              ...GraphOptions({
+                theme,
+                tooltipText: ({ value }) =>
+                  `${Number(value).toLocaleString(undefined, {
+                    maximumFractionDigits: 2,
+                    minimumFractionDigits: 2,
+                  })} ${
+                    /*selectedPST.ticker + (graphMode === "price" ? "/AR" : "")*/
+                    ""
+                  }`,
+              }),
+              maintainAspectRatio: false,
+            }}
+          />
+        </div>
+        <Card className={styles.SwapForm}>
+          {/** If the country is blocked, we display an overlay */}
+          <AnimatePresence>
+            {blockedCountry && (
+              <motion.div
+                className={
+                  styles.BlockedCountry +
+                  " " +
+                  (theme === "Dark" ? styles.BlockedCountryDark : "")
+                }
+              >
+                <div>
+                  <h1>We do not offer services in your location 😢</h1>
+                  <Spacer y={1} />
+                  <p>
+                    We apologize for the inconvenience but Verto {"&"} th8ta do
+                    not currently offer services to users in your country or
+                    location.
+                  </p>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div
+            className={
+              styles.Head + " " + (theme === "Dark" ? styles.DarkHead : "")
+            }
+          >
+            <div className={styles.HeadText}>
+              I have
+              <div className={styles.Select}>
+                <select>
+                  <option value="ARDRIVE">ARDRIVE</option>
+                </select>
+                <ChevronDownIcon className={styles.Arrow} />
+              </div>
+            </div>
+            <div className={styles.HeadText}>
+              I want
+              <div className={styles.Select}>
+                <select>
+                  <option value="VRT">VRT</option>
+                </select>
+                <ChevronDownIcon className={styles.Arrow} />
+              </div>
+            </div>
+          </div>
+        </Card>
+      </div>
       <Spacer y={4} />
       <h1 className="Title">
         Orderbook
