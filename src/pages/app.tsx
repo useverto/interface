5539c44c5536b8bb56c07b19bebd84cfc478f3a2
Marton Lederer <marton@lederer.hu>
2022-03-19 15:17:17 +0100

    feat: app balances revamp wip

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -36,18 +36,20 @@ import ListingModal from "../components/ListingModal";
 import styles from "../styles/views/app.module.sass";
 
 const App = () => {
-  const [balances, setBalances] = useState<UserBalance[]>([]);
+  //const [balances, setBalances] = useState<UserBalance[]>([]);
   const address = useSelector((state: RootState) => state.addressReducer);
   const [showMorePsts, setShowMorePsts] = useState(false);
   const theme = useTheme();
-  const [owned, setOwned] = useState([]);
+  /*const [owned, setOwned] = useState([]);
   const [userData, setUserData] = useState<UserInterface>();
-  const [loadingOwned, setLoadingOwned] = useState(true);
+  const [loadingOwned, setLoadingOwned] = useState(true);*/
+
   const router = useRouter();
   const listModal = useModal();
   const { setToast } = useToasts();
 
-  useEffect(() => {
+  // load balances
+  /*useEffect(() => {
     if (!address) return;
     setBalances([]);
     setOwned([]);
@@ -118,7 +120,71 @@ const App = () => {
             .sort((a, b) => b.balance - a.balance)
         );
     })();
+  }, [address]);*/
+
+  // load current user
+  const [user, setUser] = useState<UserInterface | null>();
+
+  useEffect(() => {
+    if (!address) return;
+    client.user.getUser(address).then((res) => setUser(res ?? null));
   }, [address]);
+
+  // load balances for user
+  const [balances, setBalances] = useState<UserBalance[]>();
+
+  useEffect(() => {
+    if (!address) return;
+
+    client.user
+      .getBalances(user?.username ?? address, "community")
+      .then((res) => setBalances(res));
+  }, [user, address]);
+
+  // preload logos of token balances
+  const [balanceLogos, setBalanceLogos] = useState<
+    {
+      id: string;
+      // should the app use the logo from the token
+      // contract or from the cryptometa api
+      useContractLogo: boolean;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      if (!balances) return;
+
+      // check if cryptometa includes the logo for
+      // the token. if it doees not, we will use the
+      // logo from the contract
+      // if the contract does not include a logo
+      // we use the placeholder provided by cryptometa
+      for (const token of balances) {
+        const cryptometaURI = `https://meta.viewblock.io/AR.${token.contractId}/logo`;
+        const res = await axios.get(cryptometaURI);
+
+        setBalanceLogos((val) => [
+          ...val,
+          {
+            id: token.contractId,
+            useContractLogo: res.status === 200 || !token.logo,
+          },
+        ]);
+      }
+    })();
+  }, [balances]);
+
+  // load owned arts for user
+  const [owned, setOwned] = useState<UserBalance[]>([]);
+
+  useEffect(() => {
+    if (!address) return;
+
+    client.user
+      .getBalances(user?.username || address, "art")
+      .then((res) => setOwned(res));
+  }, [user, address]);
 
   return (
     <Page>
@@ -138,7 +204,7 @@ const App = () => {
             <button
               className="Btn"
               onClick={() => {
-                if (!userData)
+                if (!user)
                   return setToast({
                     description: "Please setup your Verto ID first",
                     type: "error",
@@ -154,25 +220,34 @@ const App = () => {
       </h1>
       <Spacer y={2} />
       <AnimatePresence>
-        {balances.map(
-          (item, i) =>
-            (showMorePsts || i < 4) && (
-              <motion.div key={i} {...cardListAnimation(i)}>
-                <Card.Balance
-                  id={item.contractId}
-                  name={item.name}
-                  // @ts-ignore
-                  ticker={item.ticker ?? ""}
-                  balance={item.balance}
-                  logo={{
-                    dark: `https://meta.viewblock.io/AR.${item.contractId}/logo?t=dark`,
-                    light: `https://meta.viewblock.io/AR.${item.contractId}/logo?t=light`,
-                  }}
-                />
-                <Spacer y={1.5} />
-              </motion.div>
-            )
-        )}
+        {balances
+          .sort((a, b) => b.balance - a.balance)
+          .map(
+            (item, i) =>
+              (showMorePsts || i < 4) && (
+                <motion.div key={i} {...cardListAnimation(i)}>
+                  <Card.Balance
+                    id={item.contractId}
+                    name={item.name}
+                    // @ts-ignore
+                    ticker={item.ticker ?? ""}
+                    balance={item.balance}
+                    logo={
+                      balanceLogos.find(({ id }) => id === item.contractId)
+                        ?.useContractLogo
+                        ? {
+                            light: `https://arweave.net/${item.logo}`,
+                          }
+                        : {
+                            dark: `https://meta.viewblock.io/AR.${item.contractId}/logo?t=dark`,
+                            light: `https://meta.viewblock.io/AR.${item.contractId}/logo?t=light`,
+                          }
+                    }
+                  />
+                  <Spacer y={1.5} />
+                </motion.div>
+              )
+          )}
       </AnimatePresence>
       <AnimatePresence>
         {balances.length > 4 && (
@@ -218,14 +293,16 @@ const App = () => {
                 <Card.Asset
                   name={collectible.name}
                   userData={{
-                    avatar: collectible.owner.image,
-                    name: collectible.owner.name,
-                    usertag: collectible.owner.username,
+                    avatar: collectible.lister.image,
+                    name: collectible.lister.name,
+                    usertag: collectible.lister.username,
                   }}
                   price={collectible.price ?? 0}
-                  image={`https://arweave.net/${collectible.id}`}
+                  image={`https://arweave.net/${collectible.contractId}`}
                   reverse={theme === "Light"}
-                  onClick={() => router.push(`/space/${collectible.id}`)}
+                  onClick={() =>
+                    router.push(`/space/${collectible.contractId}`)
+                  }
                 />
               </motion.div>
             ))}
@@ -233,7 +310,7 @@ const App = () => {
           <AnimatePresence>
             {owned.length > 0 && (
               <motion.div className={styles.ViewAll} {...opacityAnimation()}>
-                <Link href={`/@${userData?.username ?? address}/owns`}>
+                <Link href={`/@${user?.username ?? address}/owns`}>
                   <a>
                     View all
                     <ArrowRightIcon />
@@ -242,12 +319,13 @@ const App = () => {
               </motion.div>
             )}
           </AnimatePresence>
-          {!loadingOwned && owned.length === 0 && (
+          {(owned && owned.length === 0 && (
             <p className={styles.NoOwned}>
               You do not own any collectibles. <br />
               Consider checking out <Link href="/space">Space</Link>
             </p>
-          )}
+          )) ||
+            (!owned && <>{/** TODO: loading animation here */}</>)}
           {/** Placeholder */}
           {owned.length === 0 && (
             <Card.Asset
