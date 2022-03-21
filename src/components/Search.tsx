5124adcef62e6509fa154137203f9a72f3a34f3b
Marton Lederer <marton@lederer.hu>
2022-03-21 11:46:21 +0100

    feat: search suggestions

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,18 +1,22 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { opacityAnimation } from "../utils/animations";
+import { cardListAnimation, opacityAnimation } from "../utils/animations";
 import { MenuIcon, SearchIcon, ShareIcon } from "@iconicicons/react";
 import { useState, useEffect } from "react";
-import { useTheme, Spacer, generateAvatarGradient } from "@verto/ui";
+import { useTheme } from "@verto/ui";
 import { CACHE_URL, gateway, verto } from "../utils/arweave";
+import {
+  fetchRandomCommunitiesWithMetadata,
+  fetchTopCommunities,
+} from "verto-cache-interface";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroll-component";
 import searchInputStyles from "../styles/components/SearchInput.module.sass";
 import styles from "../styles/components/Search.module.sass";
 
 export default function Search({ open, setOpen }) {
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
   const theme = useTheme();
   const router = useRouter();
 
@@ -24,22 +28,24 @@ export default function Search({ open, setOpen }) {
   }, [open, router.query]);
 
   const [page, setPage] = useState(0);
-  const [noMore, setNoMore] = useState(false);
 
   useEffect(() => {
     setPage(0);
-    setNoMore(false);
-    loadMore(true);
+    setHasMore(true);
+    setResults([]);
+    loadMore();
   }, [query]);
 
-  async function loadMore(initial = false) {
+  // infinite load results
+  const [results, setResults] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  async function loadMore() {
     if (query === "") return setResults([]);
 
-    let { data } = await axios.get(
-      `${CACHE_URL}/site/search/${query}/${initial ? 0 : page}`
-    );
+    let { data } = await axios.get(`${CACHE_URL}/site/search/${query}/${page}`);
 
-    if (data.length === 0) return setNoMore(true);
+    if (data.length === 0) return setHasMore(false);
 
     data = await Promise.all(
       data.map(async (val) => {
@@ -61,12 +67,27 @@ export default function Search({ open, setOpen }) {
       })
     );
 
+    setResults((val) => [...val, ...data]);
     setPage((val) => val + 1);
-    setResults((val) => {
-      if (initial) return data;
-      return [...val, ...data];
-    });
   }
+
+  // load featured tags from top communities
+  const [featuredTags, setFeaturedTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const topCommunities = await fetchTopCommunities(5);
+      let randomCommunities = await fetchRandomCommunitiesWithMetadata(10);
+
+      randomCommunities = randomCommunities
+        .filter(({ id }) => !topCommunities.find((val) => val.id === id))
+        .slice(0, 5);
+
+      setFeaturedTags(
+        [...topCommunities, ...randomCommunities].map(({ name }) => name)
+      );
+    })();
+  }, [open]);
 
   return (
     <AnimatePresence>
@@ -91,114 +112,116 @@ export default function Search({ open, setOpen }) {
                 autoFocus
               />
             </div>
-            <AnimatePresence>
-              {query !== "" && (
-                <motion.div
-                  {...opacityAnimation()}
-                  className={
-                    styles.Results +
-                    " " +
-                    (theme === "Dark" ? styles.DarkResults : "")
-                  }
-                >
-                  <AnimatePresence>
-                    {results.map((item, i) => (
-                      <Link
-                        href={
-                          item.type === "user"
-                            ? `/@${item.username}`
-                            : `/space/${item.id}`
-                        }
+            <InfiniteScroll
+              dataLength={results.length}
+              next={loadMore}
+              hasMore={hasMore}
+              loader={<></>}
+              style={{ overflow: "unset !important" }}
+              className={
+                styles.Results +
+                " " +
+                (theme === "Dark" ? styles.DarkResults : "")
+              }
+              endMessage={<p>You have reached the end :)</p>}
+              height={(results.length > 0 && results.length * 85) || 100}
+            >
+              <AnimatePresence>
+                {query === "" && (
+                  <div className={styles.Featured}>
+                    {featuredTags.map((tag, i) => (
+                      <motion.div
+                        className={styles.FeaturedTag}
+                        onClick={() => setQuery(tag)}
+                        {...cardListAnimation(i)}
                         key={i}
                       >
-                        <motion.a
-                          className={styles.ResultItem}
-                          {...opacityAnimation()}
-                          href={
-                            item.type === "user"
-                              ? `/@${item.username}`
-                              : `/space/${item.id}`
-                          }
-                          onClick={() => setOpen(false)}
-                        >
-                          <div className={styles.TokenData}>
-                            {(item.type !== "collection" &&
-                              (item.image || item.type !== "user") && (
-                                <img
-                                  src={
-                                    item.image?.replace(
-                                      "light",
-                                      theme.toLowerCase()
-                                    ) ?? "/arweave.png"
-                                  }
-                                  alt={item.name}
-                                  draggable={false}
-                                  className={
-                                    item.type === "user"
-                                      ? styles.UserAvatar
-                                      : ""
-                                  }
-                                  key={i}
-                                />
-                              )) ||
-                              (item.type === "user" &&
-                                !item.image &&
-                                item.gradient && (
-                                  <div
-                                    className={styles.NoAvatar}
-                                    style={{
-                                      background: item.gradient?.gradient ?? "",
-                                    }}
-                                  >
-                                    <span>
-                                      {(item.username || "")
-                                        .charAt(0)
-                                        .toUpperCase()}
-                                    </span>
-                                  </div>
-                                )) || (
-                                <MenuIcon className={styles.CollectionIcon} />
-                              )}
-                            <div>
-                              <h1>{item.name}</h1>
-                              <h2
-                                className={
-                                  item.type === "user" ? styles.UserTag : ""
-                                }
-                              >
-                                {item.type === "user"
-                                  ? `@${item.username}`
-                                  : item.ticker ?? "Collection"}
-                              </h2>
-                            </div>
-                          </div>
-                          <ShareIcon />
-                        </motion.a>
-                      </Link>
+                        {tag}
+                      </motion.div>
                     ))}
-                  </AnimatePresence>
-                  <AnimatePresence>
-                    {results.length === 0 && query !== "" && (
-                      <motion.p {...opacityAnimation()}>
-                        No results found.
-                      </motion.p>
-                    )}
-                  </AnimatePresence>
-                  {!noMore && results.length > 0 && (
-                    <>
-                      <Spacer y={1} />
-                      <div
-                        className={styles.LoadMore}
-                        onClick={() => loadMore()}
-                      >
-                        See more
+                  </div>
+                )}
+              </AnimatePresence>
+              <AnimatePresence>
+                {results.map((item, i) => (
+                  <Link
+                    href={
+                      item.type === "user"
+                        ? `/@${item.username}`
+                        : `/space/${item.id}`
+                    }
+                    key={i}
+                  >
+                    <motion.a
+                      className={styles.ResultItem}
+                      {...opacityAnimation()}
+                      href={
+                        item.type === "user"
+                          ? `/@${item.username}`
+                          : `/space/${item.id}`
+                      }
+                      onClick={() => setOpen(false)}
+                    >
+                      <div className={styles.TokenData}>
+                        {(item.type !== "collection" &&
+                          (item.image || item.type !== "user") && (
+                            <img
+                              src={
+                                item.image?.replace(
+                                  "light",
+                                  theme.toLowerCase()
+                                ) ?? "/arweave.png"
+                              }
+                              alt={item.name}
+                              draggable={false}
+                              className={
+                                item.type === "user" ? styles.UserAvatar : ""
+                              }
+                              key={i}
+                            />
+                          )) ||
+                          (item.type === "user" &&
+                            !item.image &&
+                            item.gradient && (
+                              <div
+                                className={styles.NoAvatar}
+                                style={{
+                                  background: item.gradient?.gradient ?? "",
+                                }}
+                              >
+                                <span>
+                                  {(item.username || "")
+                                    .charAt(0)
+                                    .toUpperCase()}
+                                </span>
+                              </div>
+                            )) || (
+                            <MenuIcon className={styles.CollectionIcon} />
+                          )}
+                        <div>
+                          <h1>{item.name}</h1>
+                          <h2
+                            className={
+                              item.type === "user" ? styles.UserTag : ""
+                            }
+                          >
+                            {item.type === "user"
+                              ? `@${item.username}`
+                              : item.ticker ?? "Collection"}
+                          </h2>
+                        </div>
                       </div>
-                      <Spacer y={1} />
-                    </>
-                  )}
-                </motion.div>
-              )}
-            </AnimatePresence>
+                      <ShareIcon />
+                    </motion.a>
+                  </Link>
+                ))}
+              </AnimatePresence>
+              <AnimatePresence>
+                {results.length === 0 && query !== "" && (
+                  <motion.p {...opacityAnimation()}>No results found.</motion.p>
+                )}
+              </AnimatePresence>
+            </InfiniteScroll>
           </div>
         </motion.div>
       )}
@@ -208,6 +231,11 @@ export default function Search({ open, setOpen }) {
 
 export function useSearch() {
   const [open, setOpen] = useState(false);
+
+  // do not allow body scroll when search is visible
+  useEffect(() => {
+    document.body.style.overflowY = open ? "hidden" : "auto";
+  }, [open]);
 
   return { open, setOpen };
 }
